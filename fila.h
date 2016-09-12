f1549b57b691a1122944adc50e7b6103c1ed58a5
NaatyCavalcanti <naty_zoliveira@hotmail.com>
2016-09-12 11:52:59 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -5,18 +5,22 @@ typedef struct node * link;
 struct node {
   int item;
   link next;
+  link prev;
 };
 
 typedef struct {
-  link maisAntigo;
-  link maisNovo;
+  link maisEsquerda;
+  link maisDireita;
 } * FILA;
 
-link novoNo(int item, link next);
+link novoNo(int item, link next, link prev);
 FILA novaFila();
-void inserir(FILA f, int e);
-int remover(FILA f);
-void imprimirFila(FILA f);
+void inserirmaisEsquerda(FILA f, int e);
+void inserirmaisDireita(FILA f, int e);
+int removermaisEsquerda(FILA f);
+int removermaisDireita(FILA f);
+void imprimirmaisEsquerda(FILA f);
+void imprimirmaisDireita(FILA f);
 void destroiFila(FILA f);
 int filaVazia(FILA f); 
 
